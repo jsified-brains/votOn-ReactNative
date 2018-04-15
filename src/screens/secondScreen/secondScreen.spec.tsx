3ef3f22088b9d2f6e78a44c81f46776209cb6e7e
b-tiwari <b.tiwari@hotmail.com>
2018-04-15 10:46:46 -0500

    added jest configuration and unit tests for existing components

---
+++ src/screens/secondScreen/secondScreen.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SecondScreen from './secondScreen';
+import renderer from 'react-test-renderer';
+
+describe('Header', () => {
+  it('matches expected snapshot', () => {
+    const header = renderer.create(
+      <SecondScreen />
+    ).toJSON;
+    expect(header).toMatchSnapshot();
+  });
+
+  it('renders without crashing', () => {
+    const rendered = renderer.create(<SecondScreen />).toJSON();
+    expect(rendered).toBeTruthy();
+  });
+});
