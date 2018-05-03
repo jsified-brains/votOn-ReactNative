e92811cc3257e958bf721f72b6c97826b42715b4
nchaudhary <nchaudhary@homeaway.com>
2018-05-02 20:26:16 -0500

    initial commit for carousal

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView, Image} from 'react-native';
 import { AppHeader } from '../../components';
-import { Container, Content,  Text, List, ListItem, Button} from 'native-base';
+import { Container, Content,  Text, List, ListItem} from 'native-base';
      // Left, Right, Body, Header, Icon,
      // Title, Subtitle } from 'native-base';
 
@@ -15,13 +15,28 @@ export default class HomeScreen extends Component {
                 <Content>
                     <List>
                         <ListItem>
-                            <Text style={listText}>Test Content</Text>
-                            <Button
-                                onPress={() => (this.props as any).navigation.navigate('SecondScreen', {realm: 'blah'})} >
-                                <Text>Go to the Second Screen</Text>
-                            </Button>
+                            <Text style={listText}>Test Content  </Text>
+
+                            {/*<Button*/}
+                                {/*onPress={() => (this.props as any).navigation.navigate('SecondScreen', {realm: 'blah'})} >*/}
+                                {/*<Text>Second Screen</Text>*/}
+                            {/*</Button>*/}
                         </ListItem>
                     </List>
+                    <Text style={listText}>Below is the list to scroll the images  </Text>
+
+                    <ScrollView
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        scrollEventThrottle={10}
+                        pagingEnabled
+                    >
+                        {/*<Image source={require('./Users/nchaudhary/Downloads/birdhouses/750_Country House.JPG')} />*/}
+                        <Text style={listText}> Welcome to React Native this is 1 </Text>
+                        <Text style={listText}> Welcome to React Native 2</Text>
+                        <Text style={listText}> Welcome to React Native 3 </Text>
+                        <Image source={require('/Users/nchaudhary/Downloads/birdhouses/782.JPG')} />
+                    </ScrollView>
                 </Content>
             </Container>
         );
